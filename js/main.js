1d8294d0adb79194a50dacec054dd05fbd723073
Qiu, Jason <jasonqiu0215@gmail.com>
2017-07-23 23:17:28 -0700

    final touch. fixed " no response " when change unit modes.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ document.getElementById('output').style.visibility = 'hidden';
 
 
 var currentMode='pounds';
+var currentValue=0;
+var value= document.getElementById('lbsInput').value;
+var value1=document.getElementById('item1Output');
+var value2=document.getElementById('item2Output');
+var value3=document.getElementById('item3Output');
 
 var x=document.getElementById('selection').querySelectorAll('button');
 
@@ -15,32 +20,39 @@ for(var i=0;i<x.length;i++){
         console.log(currentMode);
         document.getElementById('lbsInput').placeholder="Enter "+currentMode+"...";
         prepareCards();
+        calculation();
     });
 }
 
 document.getElementById('lbsInput').addEventListener('input', function(e){
     document.getElementById('output').style.visibility = 'visible';
-    var value = e.target.value;
-    if(currentMode==='Pounds'){
-        document.getElementById('item1Output').innerHTML = value/0.0022046;
-        document.getElementById('item2Output').innerHTML = value/2.2046;
-        document.getElementById('item3Output').innerHTML = value*16;
-    }else if(currentMode==='Grams'){
-        document.getElementById('item1Output').innerHTML = value/453.5929;
-        document.getElementById('item2Output').innerHTML = value/1000;
-        document.getElementById('item3Output').innerHTML = value*28.349492;
-    }else if(currentMode==='Kilograms'){
-        document.getElementById('item1Output').innerHTML = value/453.5929;
-        document.getElementById('item2Output').innerHTML = value/1000;
-        document.getElementById('item3Output').innerHTML = value*28.349492;
-    }else{
-        document.getElementById('item1Output').innerHTML = value/453.5929;
-        document.getElementById('item2Output').innerHTML = value/1000;
-        document.getElementById('item3Output').innerHTML = value*28.349492;
-    }
+    value = e.target.value;
+    calculation();
+
 
 });
 
+function calculation(){
+
+  console.log("calculation called");
+  if(currentMode==='Pounds'){
+       value1.innerHTML= value/0.0022046;
+       value2.innerHTML= value/2.2046;
+       value3.innerHTML= value/0.0625;
+  }else if(currentMode==='Grams'){
+       value1.innerHTML= value/453.5929;
+       value2.innerHTML= value/1000;
+       value3.innerHTML= value/28.349492;
+  }else if(currentMode==='Kilograms'){
+       value1.innerHTML= value/0.4535;
+       value2.innerHTML= value/0.001;
+       value3.innerHTML= value/0.02834;
+  }else{
+       value1.innerHTML = value/16;
+       value2.innerHTML = value/0.035273;
+       value3.innerHTML = value/35.279;
+  }
+}
 
 
 
@@ -51,6 +63,7 @@ document.getElementById('lbsInput').addEventListener('input', function(e){
 
 
 function prepareCards(){
+    console.log("preparecards called");
     var item1=document.getElementById('item1');
     var item2=document.getElementById('item2');
     var item3=document.getElementById('item3');
@@ -78,8 +91,3 @@ function prepareCards(){
     }
 
 }
-
-
-
-
-
